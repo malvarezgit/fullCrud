582ddca5bfee305a45fa51958a7417bef2251f2b
malvarezgit <100394712+malvarezgit@users.noreply.github.com>
2022-02-25 12:03:00 -0300

    Configuracion CORS para proyecto Angular

--- backcrud/src/main/java/cl/malvarez/spring/backcrud/controller/ControllerPerson.java
+++ backcrud/src/main/java/cl/malvarez/spring/backcrud/controller/ControllerPerson.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import cl.malvarez.spring.backcrud.entity.Person;
 import cl.malvarez.spring.backcrud.entity.Phone;
 import cl.malvarez.spring.backcrud.service.ServicePerson;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 @Validated
